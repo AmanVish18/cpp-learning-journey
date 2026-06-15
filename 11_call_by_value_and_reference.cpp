2f7e7461e43fbae8bd205c3b345015519317a1f4
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:43:24 +0530

    Create 11_call_by_value_and_reference.cpp

---
+++ 11_call_by_value_and_reference.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std ;
+//call by value
+/*void sum(int a,int b){
+int s=a;
+a=b;
+b=s;
+}*/
+//call by reference by using pointers
+/*void pointsum(int* a,int* b){
+int s=*a;
+*a=*b;
+*b=s;
+}*/
+void refesum( int &a,int &b){
+int s=a;
+a=b;
+b=s;
+}
+
+int main(){
+int x=9,y=5;
+cout << "the value of x is:" <<x<< endl;
+cout << "the value of y is:"<<y << endl;
+/*sum(x,y);
+cout << "the value of x is:" <<x<< endl;
+cout << "the value of y is:"<<y << endl;*/
+/*pointsum(&x,&y);
+cout << "the value of x is:" <<x<< endl;
+cout << "the value of y is:"<<y << endl;*/
+refesum(x,y);
+cout << "the value of x is:" <<x<< endl;
+cout << "the value of y is:"<<y << endl;
+return 0 ;
+}
+Added call by value and call by reference examples
