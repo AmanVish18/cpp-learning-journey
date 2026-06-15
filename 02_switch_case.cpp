f2668bad88922ec48f5e3b77dc6fff1d17d3a10b
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:33:24 +0530

    Create 02_switch_case.cpp

---
+++ 02_switch_case.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age;
+//enjoy my code
+cout << "apniiii age bata chikne🤧??" << endl;
+cin >> age;
+//switch case statement 
+switch(age)
+{ case 5:
+cout << "you are nibbaaaaa😏." << endl;
+break;
+case 10:
+cout << "Abhii bada ho rha hai tu 😌" << endl;
+break;
+case 18:
+cout << "babuuuuuuuu🎀❤️" << endl;
+break;
+default:
+cout << "toooo mai kya kruuuuu😏." << endl;
+break;
+}
+return 0 ;
+}
+Added switch case practice program
