098ef0446b6e9e8e795eab3cb50f6c3230096ffa
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:30:55 +0530

    Create 01_if_else.cpp

---
+++ 01_if_else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+// uses of if and else 
+cout<<"this is another day of my c++ journey"<<endl;
+int age;
+cout << endl;
+cout << "age??" << endl;
+cin >>age;
+if(age<18){
+cout<<"you can't perticipate in party."<<endl;
+}
+else if(age==18){
+cout << "you can perticipate but with pass." << endl;
+}
+else{
+cout << "now enjoy party." << endl;
+}
+Added if-else practice program
