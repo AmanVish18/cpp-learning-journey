0304839e76eac3a22ec08428f137cad0e005335a
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:35:46 +0530

    Create 04_switch_password.cpp

---
+++ 04_switch_password.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){  
+int code;
+
+      cout << "What is my age?🤨" << endl;
+      cin>> code;
+
+switch(code)
+
+{case 17:
+    cout << "password:sulululuuuuuu " << endl;
+    
+    break;
+ default:
+       cout << " incorrect." << endl;
+       break;
+    }
+    return 0;
+    }
+Added switch case password checker practice
