281b146fae25c6fca24df82578977ad4cca49562
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:36:47 +0530

    Create 05_break_continue.cpp

---
+++ 05_break_continue.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//break and continue in C++
+    cout << "your coder aman🎀!" << endl;
+ cout  << endl;
+ for(int a=2;a<30;a=a+1){
+  
+if(a==10){
+     continue;
+    }
+     
+     cout << "I ❤️ U..." <<a<< endl;
+ 
+ 
+  if(a==25)
+         
+         break;
+     
+  }  
+    
+    return 0;
+}
+Added break and continue statement practice
