93e9deccc56753b4d91b2818fd8eb1603d3c9577
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:45:19 +0530

    Create 13_default_arguments.cpp

---
+++ 13_default_arguments.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+//default argument 
+float forsure(float money,float factor=1.08){
+   return money*factor ;
+}
+int main(){
+cout << "Enter your investing price!!" << endl;
+float a;
+cin >>a;
+      cout << "if you invest: $" <<a<<", you will get return rate: " <<forsure(a)<<endl;
+
+      cout << "but for VIP : $" <<a<<", they will get return rate: " <<forsure(a,1.20)<<endl;
+return 0 ;
+}
+Added default arguments function practice
