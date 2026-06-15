454564ba981efc19612f73d68a4e669fe8a694c5
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:34:38 +0530

    Create 03_loops.cpp

---
+++ 03_loops.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+/* loops in c++
+1.for loop*/
+/*for(int a=1;a<=9789;a=a*2)
+
+{ 
+   cout<<a<<endl;
+
+}
+//infinity loop 
+for(float a=1.2;2<3;a++){
+       
+       cout<<a;
+
+}
+//while loop
+float a=0.5;
+while(a<=99){
+    cout<<a<<endl;
+    a=a+0.6;
+}
+//infinity while loop
+int a=12;
+while(true){
+     
+     cout<<a;
+     a=0.5;
+
+}
+//do while loop 
+int a=1;
+do{
+    cout<<"Babuuuuuu..❤️"<<a<<endl;
+    a=a+1;
+}while(a<=50);*/
+ 
+ // infinity do while loop 
+ int a=0;
+ do{
+    cout << "melaaa bachaaaaa❤️🎀"<<a<< endl;
+    a=a+1;
+ }while(true);
+return 0;
+}
+Added loops practice with for, while and do-while
