b070ea8463900f7e78e4aade2627344d2a8a4e95
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:44:32 +0530

    Create 12_inline_function_modulus.cpp

---
+++ 12_inline_function_modulus.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std ;
+//use of inline
+inline int mod(int a,int b){
+if(b == 0)
+{
+   cout << "Modulus by zero error!" << endl;
+        return -2;
+}
+
+if(a < b)
+ {
+   cout << "Take bigger value of a!!" << endl;
+        return -1;
+}
+
+    return a % b;
+}
+int main(){
+int a,b;
+
+cout <<"enter the value of a: "<< endl;
+cin >> a;
+cout << "enter the value of b: " << endl;
+cin >> b;
+int ans=mod(a,b);
+if(ans==-1){
+return 0;}
+else if(b==0){
+return 0;
+}
+else{
+cout <<"your answer: "<<ans<< endl;
+cout <<"your answer: "<<(ans+1)<< endl;
+cout <<"your answer: "<<(ans+2)<< endl;
+cout <<"your answer: "<<(ans+3)<< endl;
+}
+return 0 ;
+}
+Added inline modulus function with error handling
