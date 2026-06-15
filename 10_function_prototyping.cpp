799f010c6f4ab0cb628a6e1eca7a60aa3a7a1f50
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-15 09:42:28 +0530

    Create 10_function_prototyping.cpp

---
+++ 10_function_prototyping.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std ;
+int sum(int a,int b){
+int c=a+b;
+
+return c;
+}
+float mod( float x,float y);
+int main(){
+int num1,num2;
+float num3,num4;
+void g();
+
+cout << "enter fhe valuse of num1:" << endl;
+cin>>num1;
+cout << "enter the value of num2:" << endl;
+cin >> num2;
+cout <<sum(num1,num2)<< endl;
+cout << "enter the value of num3: " << endl;
+cin >> num3;
+cout << "enter the value of num4: " << endl;
+cin >> num4;
+cout <<mod(num3,num4) << endl;
+cout << " Bada hossyarrr hai addition and multiplication bhii aise kr rha gadhe😂" << endl;
+g();
+return 0;
+
+
+}
+//function prototyping 
+float mod( float x,float y){
+float z=x*y;
+return z;
+}
+void g(){
+
+cout << "suluuuluuuuuuu🫣" << endl; 
+}
+Added function prototyping and void function practice
