e00115328916189594ccf0f95d2e71b3d9a45582
AmanVish18 <vishwakarmaaman7905@gmail.com>
2026-06-17 21:49:49 +0530

    Create 22_complex_number_friend_function.cpp
    feat: add complex number addition using friend function

---
+++ 22_complex_number_friend_function.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std ;
+class complex {
+    int a,b;
+public :
+    friend complex sumdata(complex o1,complex o2);
+    void setdata(void) {
+        int n1,n2;
+        cout << "enter the value of:" << endl;
+        cin >>n1;
+        cout << "enter the value of second one :" << endl;
+        cin >> n2;
+        a=n1;
+        b=n2;
+    }
+    void display(void) {
+        cout << " your complex number is: " <<a<<" + "<<b<< "i"<<endl;
+
+    }
+};
+complex sumdata(complex o1,complex o2) {
+
+    complex o3;
+    o3.a=((o1.a+o2.a));
+    o3.b=((o1.b+o2.b));
+    return o3;
+}
+int main() {
+    complex c1,c2,c3;
+    c1.setdata();
+    c1.display();
+
+    c2.setdata();
+    c2.display();
+
+    c3=sumdata(c1,c2);
+    c3.display();
+
+    return 0;
+
+}
